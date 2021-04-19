e6e45a6a6fec247bb07955e1c76c44f9d0ed3201
acferM <matheus.acfer@nave.org.br>
2021-04-19 19:53:13 -0300

    app done

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,55 @@
+import { signIn, useSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
+import { Session } from 'next-auth'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
+import styles from './styles.module.scss'
+
+interface SubscribeButtonProps {
+  priceId: string
+}
+
+export interface UserSubscriptionSession  extends Session {
+  activeSubscription?: any
+}
+
+type SessionProps = [UserSubscriptionSession, boolean]
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session]: SessionProps = useSession()
+  const router = useRouter()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe') 
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch(err) {
+      alert(err.message)
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  )
+}
